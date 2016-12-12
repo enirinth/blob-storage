60853f89795e165521a1568329606035e32ee76c
Ray Chen <raychen0411@gmail.com>
2016-12-12 00:52:01 -0500

    add more running commands for centralized dc

--- client/read-client/run_read_rand_central.sh
+++ client/read-client/run_read_rand_central.sh
@@ -24,5 +24,17 @@ go run central_manager_read_client.go $1 40
 echo "third test, 50 clients"
 go run central_manager_read_client.go $1 50
 
+echo "third test, 60 clients"
+go run central_manager_read_client.go $1 60
+
 echo "third test, 80 clients"
 go run central_manager_read_client.go $1 80
+
+echo "third test, 100 clients"
+go run central_manager_read_client.go $1 100
+
+echo "third test, 120 clients"
+go run central_manager_read_client.go $1 120
+
+echo "third test, 150 clients"
+go run central_manager_read_client.go $1 150
