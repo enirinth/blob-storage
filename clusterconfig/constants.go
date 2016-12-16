df21f24103ca60a4450a826d106e29d359990d76
Ray Chen <raychen0411@gmail.com>
2016-12-16 04:24:16 -0500

    switch back to hot cooy

--- clusterconfig/constants.go
+++ clusterconfig/constants.go
@@ -21,12 +21,12 @@ const (
 	PopulateServiceOn   bool          = true
 	SyncServiceOn       bool          = true
 	PrintServiceOn      bool          = false
-	CopyEveryWhereOn    bool          = true
+	CopyEveryWhereOn    bool          = false
 	TCON                bool          = true
 	MockTransLatencyON  bool          = true
 	readReqFiles        int           = 10
-	PopulateInfFactor   int           = 80
-	SyncInfFactor       int           = 20
+	PopulateInfFactor   int           = 40
+	SyncInfFactor       int           = 5
 )
 
 // Networking configuration
